deded7f446e9b07bc8f14030a6c98af95de58939
amandatquan <116510061+amandatquan@users.noreply.github.com>
2024-03-18 19:47:19 -0700

    Add files via upload

--- Final Project R Code.R
+++ Final Project R Code.R
@@ -23,13 +23,13 @@ ERIMDataOmit = na.omit(ERIMData)
 # Note: using ERIMDATA, not Ommission_Data
 # Updated to exclude the N/A data
 
-# ----------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
 # SUBSETTING THE DATA 
 # Subseting Residence Status & Education Level (Male & Female) 
 myData1 = ERIMDataOmit[,c("ResStatus","MEdu")]
 myData2 = ERIMDataOmit[,c("ResStatus","FEdu")]
 
-# ----------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
 # Create Custom Binning the Data Based the Education level to make it easier to interpret
 # Subsetting Male Education
 myData1$Binning = cut(ERIMDataOmit$MEdu, breaks = c(-Inf, 8, 9, 10, 11),
@@ -107,10 +107,16 @@ summary(ERIMDataOmit$FWrkHrs) # Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
 
 length(which(ERIMDataOmit$ResStatus == 3)) # 40
 
-ERIMDataOmit$ResidencyStatus = cut(ERIMDataOmit$ResStatus, breaks = c(0, 1, 3),
-                    labels = c("1", "0"))
-
 ERIMDataOmit$homeOwner <- ifelse(ERIMDataOmit$ResStatus == 1, 1, 0)
+
+
+# bar graph of the residency status
+counts = table(ERIMDataOmit$ResStatus)
+barplot(counts, main = "Barplot Distribution of Residency Status",
+        xlab = "Residency Status",
+        ylab = "# of People", 
+        ylim = c(0, 3000),
+        col= c("blue", "green", "red"))
 
 
 # We want to classify residency status into binary variables (1 = owned home, 2 = other aka renting, etc.)
@@ -135,63 +141,27 @@ status_test5 = 0.6488107 + (0.0384782 * 9) + (-0.0010606 * 100)
 status_test6 = 0.6692530 + (0.0244472 * 9) + (0.0006898 * 40) + (0.0075484 * 9) + (-0.0008819 * 40)
 
 
-# Includes household members
-model6 <- lm(ResStatus ~ MEdu + MWrkHrs + FEdu + FWrkHrs + HHNbr, data = ERIMDataOmit)
-summary(model6)
-
-
-# not sure if we are supposed to use logistic regression....
-model6_log = glm(ResStatus ~ MEdu + MWrkHrs + FEdu + FWrkHrs + HHNbr, data = ERIMDataOmit)
-summary(model5_log)
-
-
 # -------------------------------------------------------------------------------------------------------
-# MODELING 3 --- Determining whether income influences dog ownership
+# MODELING 3 --- We want to determine whether income is determines whether a household is likely to own a
 
-# Determine whether income influences the amount of dogs they own.
-dog_model = lm(HHInc ~ Dogs, data = ERIMDataOmit)
-summary(dog_model)
-options(scipen = 999)
-# dog_model = 5.75113 + (0.52599 * dogs)
-
-
-dog_model2 = lm(HHInc ~ Dogs + ResType, data = ERIMDataOmit)
-summary(dog_model2)
-# dog_model2 = 4.39643 + (0.48729 * dogs) + (0.46394 * ResType)
-
-
-# ------------------------------------------------------------------------------------------------------
-# Question: Does Income, Residency status, and residency type affect whether a household owns a dog. 
-
-
-# MODEL 3 -- UPDATED
-dog_model3 = lm(Dogs ~ HHInc + ResStatus + ResType, data = ERIMDataOmit)
-summary(dog_model3)
-# dog_model3 = 0.13605 + (0.02956 * income) + (-0.06014 * ResStatus) + (0.08155 * ResType)
-# standard error = 0.7326, adjusted r-squared = 0.02434
-
-
-dog_model4 = lm(Dogs ~ HHInc + ResType, data = ERIMDataOmit)
-summary(dog_model4)
-# dog_model4 = 0.13605 + (0.031515 * income) + (0.093637 * ResType)
-# standard error = 0.7328, adjusted r-squared = 0.02378
-
-
-
-
-
-# ----------------------------------------------------------------------------------------------------------------
-# BULL SHIT WE NEED TO FIX ---- 
 
 # Subseting Income 
-myData3 = ERIMDataOmit[,c("HHInc", "HHNbr", "MWrkHrs", "FWrkHrs", "Cats", "Dogs")]
+myData3 = ERIMDataOmit[,c("HHInc", "HHNbr", "MWrkHrs", "FWrkHrs", "Cats", "Dogs", "ResStatus", "ResType")]
 myData3$Pets = myData3$Cats + myData3$Dogs
-
 
 myData3$Income = cut(ERIMDataOmit$HHInc, breaks = c(-Inf, 6, 9, 14),
                      labels = c("Low Income", "Middle Income", "High Income"))
 
 myData4 = myData3[myData3$Pets == 1 & myData3$Income == "High Income",]
+
+# we want to know how many cats and dogs are owned by each income level
+counts1 = table(myData3$Income)
+barplot(counts1, main = "Pet Ownership",
+        xlab = "Income Levels", 
+        ylab = "# of Pets Owned",
+        ylim = c(0, 2000),
+        col= c("purple", "orange", "pink"))
+
 
 # KEEP FOR NOW --
 # length(which(myData3$Income == "High Income" & myData3$Pets <= 2)) # 406
@@ -200,15 +170,18 @@ myData4 = myData3[myData3$Pets == 1 & myData3$Income == "High Income",]
 # length(which(myData3$Income == "Low Income" & myData3$Pets >= 4)) # 55
 
 
-boxplot(myData3$HHInc,
-        main = "Boxplot for High Income Pet Owners",
-        xlab = "Number of Pets",
-        names = c("Income"),
-        horizontal = TRUE,
-        col = "blue")
+cat_model = lm(Cats ~ HHInc + ResStatus + ResType, data = ERIMDataOmit)
+summary(cat_model)
+# standard error = 0.7953, adjusted r^2 = 0.003115
 
-myData3$Numeric_Income = as.numeric(myData3$HHInc * 10000)
-plot(Pets ~ Numeric_Income, data = myData3)
+dog_model = lm(Dogs ~ HHInc + ResStatus + ResType, data = ERIMDataOmit)
+summary(dog_model)
+# standard error = 0.7326, Adjusted R-squared:  0.02434
+
+pet = lm(Pets ~ HHInc + ResStatus + ResType, data = myData4)
+summary(pet)
+
+
 
 
 
